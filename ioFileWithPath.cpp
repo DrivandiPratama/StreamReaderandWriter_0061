87ccca9f1d4696a2954270b595f616f90792d643
DrivandiPratama <dripandipratama@gmail.com>
2025-06-10 15:40:22 +0700

    commit 24

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjukkan ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    // unlimited loop untuk menulis
   
 }
